a7de9f4d47c9131405ca30b3164012a49e3f8835
patriciadhl <patricia.dione@gmail.com>
2018-01-27 17:35:20 -0600

    actualizando

--- js/app.js
+++ js/app.js
@@ -39,7 +39,8 @@ document.getElementById('botonlogout').addEventListener('click', function() {
 authService.onAuthStateChanged(function(user) {
   if (user) {
     console.log('AuthStateChanged', user)
-    document.getElementById('datosuser').innerHTML = JSON.displayName;
+    document.getElementById('datosuser').innerHTML = JSON.stringify(user);
+    console.log(JSON)
     document.getElementById('botonlogin').style.display = 'none';
     document.getElementById('botonlogout').style.display = 'block';
   } else {
